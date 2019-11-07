774ccedb6d4df32e0844f54e402a7ae730523f23
Marissa Culbreth <mculbreth1996@ufl.edu>
2019-11-07 00:09:17 -0500

    Fix hamburger menu

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 // Hamburger Menu
+var icon = document.getElementById("hamIcon");
+
+function toggleMenu() {
+  this.nextElementSibling.classList.toggle("mobileMenu");
+}
+
+icon.addEventListener("click", toggleMenu);
 
 
 
@@ -20,21 +27,40 @@ for (i = 0; i < coll.length; i++) {
 
 
 // Read more buttons on contact page
-function readMoreButton() {
-  var dots = document.getElementsByClassName("dots");
-  var moreText = document.getElementsByClassName("more");
-  var btnText = document.getElementsByClassName("readMore");
+var i = 0;
 
-  if (dots.style.display === "none") {
-    dots.style.display = "inline";
-    btnText.innerHTML = "READ MORE";
-    moreText.style.display = "none";
-  } else {
-    dots.style.display = "none";
-    btnText.innerHTML = "READ LESS";
-    moreText.style.display = "inline";
+function read() {
+  if (!i) {
+    document.getElementsByClassName("more").style.display = "none";
+    document.getElementsByClassName("dots").style.display = "inline";
+    document.getElementsByClassName("readMore").innerHTML="READ LESS";
+    i=1;
+  }
+  else {
+    document.getElementsByClassName("more").style.display = "inline";
+    document.getElementsByClassName("dots").style.display = "none";
+    document.getElementsByClassName("readMore").innerHTML="READ MORE";
+    i=0;
   }
 }
+
+
+
+// function myFunction() {
+//   var dots = document.getElementById("dots");
+//   var moreText = document.getElementById("more");
+//   var btnText = document.getElementById("myBtn");
+//
+//   if (dots.style.display === "none") {
+//     dots.style.display = "inline";
+//     btnText.innerHTML = "Read more";
+//     moreText.style.display = "none";
+//   } else {
+//     dots.style.display = "none";
+//     btnText.innerHTML = "Read less";
+//     moreText.style.display = "inline";
+//   }
+// }
 
 
 // API on contact page
